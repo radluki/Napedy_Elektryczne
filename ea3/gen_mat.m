575597c7081c130f421b19f6f62d48736d8b19ea
legoxx <dawidlegutki@gmail.com>
2015-06-15 16:56:01 +0200

    Dawida Commit

--- ea3/gen_mat.m
+++ ea3/gen_mat.m
@@ -7,6 +7,7 @@ convert_csv_mat(files)
 
 %}
  %% analysing AC powered motor
+ clear all;
 files = {'AC182.mat','r_tyrystorowy.mat','wyprostowane152.mat','nV.mat'};
 close all
 figure(1)
@@ -16,13 +17,13 @@ for i = 1:3
     load(files{i})
 
      % straty mechaniczne oraz w żelazie prądnicy, 
-    P0 = 1.2*n.^2*1e-6 + 1.1*n*1e-3;
+    P0 = 1.2*(n.^2)*(1e-6) + 1.1*n*(1e-3);
      % moc oddawana przez prądnicą
     Pod = Up.*Ip;
-    Pcu = 26*Ip.^2;
+    Pcu = 26*(Ip.^2);
 
     Podsil = P0 + Pcu + Pod;
-    T = Podsil./(2*pi/60*n);
+    T = Podsil./(n*pi/30);
     ni = Podsil./P;
     xx = linspace(n(1),n(end),100);
     a = polyfit(n,ni,2);
